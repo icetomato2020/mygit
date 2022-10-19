25252a4384b6c315e1d020acf318fc9963f99ff0
taorui <taorui@uplooking.com>
2022-10-19 15:31:55 +0800

    部门业务实现

---
+++ mygit/src/main/java/com/uplooking/service/impl/DeptServiceImpl.java
@@ -0,0 +1,10 @@
+package com.uplooking.service.impl;
+
+import com.uplooking.service.DeptService;
+
+public class DeptServiceImpl implements DeptService {
+    @Override
+    public void addDept() {
+        System.out.println("部门业务实现");
+    }
+}
